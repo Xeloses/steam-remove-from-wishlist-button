1bac8bb7ac6679f44e23619a5d30cdf47c841d1c
Xeloses <37206095+Xeloses@users.noreply.github.com>
2020-06-03 19:19:13 +0300

    Update steam-remove-from-favorites-button.user.js

--- steam-remove-from-favorites-button.user.js
+++ steam-remove-from-favorites-button.user.js
@@ -24,60 +24,67 @@
     // check URL:
     if (/https:\/\/store\.steampowered\.com\/app\/[\d]{2,}[\/]?[\S]*/i.test(w.location.href)) {
 
-        // get Wishlist associated elements:
-        let _wl_container = $J('.queue_actions_ctn');
-        if(!_wl_container || !_wl_container.length){return;}
+        // get user info from Steam:
+        let _userinfo = $J('#application_config').data('userinfo');
 
-        let _wl_area   = _wl_container.find('#add_to_wishlist_area');
-        let _wl_s_area = _wl_container.find('#add_to_wishlist_area_success');
-        let _wl_f_area = _wl_container.find('#add_to_wishlist_area_fail');
+        // check user has logged in:
+        if(_userinfo && _userinfo.logged_in){
 
-        // get user's Steam ID:
-        let _SteamID = $J('#application_config').data('userinfo').steamid;
-        if(!_SteamID || !/^\d{8,20}$/.test(_SteamID)){return;}
+            // get Wishlist associated elements:
+            let _wl_container = $J('.queue_actions_ctn');
+            if(!_wl_container || !_wl_container.length){return;}
 
-        // get Session ID from Steam's scripts or from Cookies:
-        let _sID = g_sessionID ? g_sessionID : document.cookie.match(/(^|;)\\s*sessionid\\s*=\\s*([^;]+)/i).pop();
+            let _wl_area   = _wl_container.find('#add_to_wishlist_area');
+            let _wl_s_area = _wl_container.find('#add_to_wishlist_area_success');
+            let _wl_f_area = _wl_container.find('#add_to_wishlist_area_fail');
 
-        // get App ID:
-        let _appID = w.location.href.match(/https:\/\/store\.steampowered\.com\/app\/[\d]{2,}/i).pop().replace(/\D*/, '');
+            // get user's Steam ID:
+            let _SteamID = _userinfo.steamid;
+            if(!_SteamID || !/^\d{8,20}$/.test(_SteamID)){return;}
 
-        // create "Remove from Wishlist" button:
-        let _xbtn = $J('<a href="javascript:;" class="btnv6_blue_hoverfade btn_medium" title="Remove from favorites." style="margin-left:2px;"><span>✘</span></a>').click(()=>{
-            $J.ajax({
-                type: 'POST',
-                url: 'https://store.steampowered.com/wishlist/profiles/' + _SteamID + '/remove/',
-			    data: {appid: _appID, sessionid: _sID},
-                success: ()=>{
-                    _wl_s_area.css('display','none');
-                    _wl_area.css('display','inline-block');
-                },
-                error: ()=>{
-                    alert('Error attempt to remove app from whishlist.');
+            // get Session ID from Steam's scripts or from Cookies:
+            let _sID = g_sessionID ? g_sessionID : document.cookie.match(/(^|;)\s*sessionid\s*=\s*([^;]+)/i).pop();
+
+            // get App ID:
+            let _appID = w.location.href.match(/\/app\/([\d]*)\//i).pop();
+
+            // create "Remove from Wishlist" button:
+            let _xbtn = $J('<a href="javascript:;" class="btnv6_blue_hoverfade btn_medium" title="Remove from favorites." style="margin-left:2px;"><span>✘</span></a>').click(()=>{
+                $J.ajax({
+                    type: 'POST',
+                    url: 'https://store.steampowered.com/wishlist/profiles/' + _SteamID + '/remove/',
+                    data: {appid: _appID, sessionid: _sID},
+                    success: ()=>{
+                        _wl_s_area.css('display','none');
+                        _wl_area.css('display','inline-block');
+                    },
+                    error: ()=>{
+                        alert('Error attempt to remove app from whishlist.');
+                    }
+                });
+            });
+
+            // check App/Game state:
+            if(!_wl_area || !_wl_area.length){
+                // App/Game in Wishlist
+                // --------------------
+                // create Wishlist associated elements:
+                _wl_area = $J('<div id="add_to_wishlist_area" style="display: none"><a class="btnv6_blue_hoverfade btn_medium" href="javascript:AddToWishlist(' + _appID + ', \'add_to_wishlist_area\', \'add_to_wishlist_area_success\', \'add_to_wishlist_area_fail\', &quot;1_store-navigation__&quot;, \'add_to_wishlist_area2\' );" title="Add to whishlist."><span>Whishlist</span></a></div>');
+                _wl_s_area = $J('<div id="add_to_wishlist_area_success"><a href="https://steamcommunity.com/id/xeloses/wishlist" class="btnv6_blue_hoverfade btn_medium queue_btn_active" title="Open Whishlist."><span><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_selected.png" border="0">Whishlisted</span></a></div>');
+                _wl_f_area = $J('<div id="add_to_wishlist_area_fail" style="display: none;"><b>Error attempt to access whishlist!</b><br />Please, retry later.</div>');
+
+                // remove default "Whishlist" button from page:
+                let _wl_btn = _wl_container.children('a.queue_btn_active').has('img');
+                if(_wl_btn && _wl_btn.length){
+                    _wl_btn.remove();
                 }
-		    });
-        });
 
-        // check App/Game state:
-        if(!_wl_area || !_wl_area.length){
-            // App/Game in Wishlist
-            // --------------------
-            // create Wishlist associated elements:
-            _wl_area = $J('<div id="add_to_wishlist_area" style="display: none"><a class="btnv6_blue_hoverfade btn_medium" href="javascript:AddToWishlist(' + _appID + ', \'add_to_wishlist_area\', \'add_to_wishlist_area_success\', \'add_to_wishlist_area_fail\', &quot;1_store-navigation__&quot;, \'add_to_wishlist_area2\' );" title="Add to whishlist."><span>Whishlist</span></a></div>');
-            _wl_s_area = $J('<div id="add_to_wishlist_area_success"><a href="https://steamcommunity.com/id/xeloses/wishlist" class="btnv6_blue_hoverfade btn_medium queue_btn_active" title="Open Whishlist."><span><img src="https://steamstore-a.akamaihd.net/public/images/v6/ico/ico_selected.png" border="0">Whishlisted</span></a></div>');
-            _wl_f_area = $J('<div id="add_to_wishlist_area_fail" style="display: none;"><b>Error attempt to access whishlist!</b><br />Please, retry later.</div>');
-
-            // remove default "Whishlist" button from page:
-            let _wl_btn = _wl_container.children('a.queue_btn_active').has('img');
-            if(_wl_btn && _wl_btn.length){
-                _wl_btn.remove();
+                // add Wishlist associated elements to page:
+                _wl_container.prepend(_wl_f_area).prepend(_wl_s_area).prepend(_wl_area);
             }
 
-            // add Wishlist associated elements to page:
-            _wl_container.prepend(_wl_f_area).prepend(_wl_s_area).prepend(_wl_area);
+            // add "Remove from Wishlist" button to page:
+            _wl_container.find('#add_to_wishlist_area_success').append(_xbtn);
         }
-
-        // add "Remove from Wishlist" button to page:
-        _wl_container.find('#add_to_wishlist_area_success').append(_xbtn);
     }
 })();
